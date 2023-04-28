ce578a4da0dfba661b8f4545ae4fe62217152a97
EthanSMorse <emorse@brooklynsteamcenter.org>
2023-04-28 14:52:09 -0400

    Bumnton

---
+++ Stopwatch.js
@@ -0,0 +1,9 @@
+function start () {
+    alert("You pressed the start button you FOOL.")
+}
+function stop () {
+    alert("You cant simply stop it. Feel the ramifications of your actions.")
+}
+function reset () {
+    alert("No. It cant be done.")
+}
