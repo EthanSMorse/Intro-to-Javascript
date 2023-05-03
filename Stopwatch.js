4580772de292b0f84c01a0a67f35efa20a466edb
EthanSMorse <emorse@brooklynsteamcenter.org>
2023-05-03 14:56:34 -0400

    Completed Stopwatch

--- Stopwatch.js
+++ Stopwatch.js
@@ -1,9 +1,70 @@
+let minutes = 00
+
+let seconds = 00
+
+let milliseconds = 0
+
+let digits = document.getElementById('digits')
+
+let startButton = document.getElementById('start_button')
+
+let interval;
+
+let lapButton = document.getElementById('lap_button')
+
+lapButton.disabled = true
+
 function start () {
-    alert("You pressed the start button you FOOL.")
+    interval = setInterval (
+        function () {
+
+            startButton.disabled = true
+
+            lapButton.disabled = false
+
+            milliseconds = milliseconds + 10
+            console.log (milliseconds)
+            if (milliseconds == 1000) {
+                seconds = seconds + 1
+                milliseconds = 0
+            }
+            console.log (seconds)
+            if (seconds ==  60) {
+                minutes = minutes + 1
+                seconds = 00
+            }
+            console.log (minutes)
+            digits.innerHTML = String(minutes).padStart(2,'0') + ':' + String(seconds).padStart(2,'0') + ':' + milliseconds / 10
+        } ,10
+    )
 }
 function stop () {
-    alert("You cant simply stop it. Feel the ramifications of your actions.")
+
+    startButton.disabled = false
+
+    lapButton.disabled = true
+
+    clearInterval (
+        interval
+    )
 }
 function reset () {
-    alert("No. It cant be done.")
+
+    startButton.disabled = false
+
+    lapButton.disabled = true
+
+    stop ()
+    minutes = 00
+    seconds = 00
+    milliseconds = 0
+    digits.innerHTML = '00:00:00'
 }
+
+function lap() {
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(String(minutes).padStart(2,'0') + ':' + String(seconds).padStart(2,'0') + ':' + milliseconds / 10);
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+}
+
